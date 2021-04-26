4093629ea2a9486bb0bb73b191c77abc1709a1cb
Your Name <you@example.com>
2021-04-27 00:55:55 +0300

    Add Tester

--- BE/Address.cs
+++ BE/Address.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BE
 {
-    public class Address     /*add*/
+    public class Address 
     {
 
         public string City { get; set; }
--- UI/Tools/MailSender.cs
+++ UI/Tools/MailSender.cs
@@ -13,17 +13,17 @@ namespace UI.Tools
     {
         public void SendMail(string to, string subject, string body)
         {
-            MailMessage message = new MailMessage();
-            message.Subject = subject;
-            message.From = new MailAddress(((App)Application.Current).Currents.MailAddress);
-            message.Body = body;
-            message.To.Add(to);
-            SmtpClient smtp = new SmtpClient("smtp.Gmail.com");
-            smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new NetworkCredential(((App)Application.Current).Currents.MailAddress, ((App)Application.Current).Currents.MailPassword);
-            smtp.EnableSsl = true;
-            smtp.Port = 587;
-            smtp.Send(message);
+            /* MailMessage message = new MailMessage();
+             message.Subject = subject;
+             message.From = new MailAddress(((App)Application.Current).Currents.MailAddress);
+             message.Body = body;
+             message.To.Add(to);
+             SmtpClient smtp = new SmtpClient("smtp.Gmail.com");
+             smtp.UseDefaultCredentials = false;
+             smtp.Credentials = new NetworkCredential(((App)Application.Current).Currents.MailAddress, ((App)Application.Current).Currents.MailPassword);
+             smtp.EnableSsl = true;
+             smtp.Port = 587;
+             smtp.Send(message);*/
 
         }
     }
